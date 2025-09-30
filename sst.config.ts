701df8bc49a07f0c4795d4f368d057bb91ad9fea
glody007 <glodymbutwile@gmail.com>
2025-09-30 09:43:39 +0200

    feat: initialize next js and provision infra with cognito

---
+++ sst.config.ts
@@ -0,0 +1,35 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "adona-sst",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+      providers: {
+        aws: {
+          profile: "adona-sst-profile",
+          region: "us-east-1",
+        },
+      },
+    };
+  },
+  async run() {
+    const userPool = new sst.aws.CognitoUserPool("UserPool", {
+      usernames: ["email"]
+    });
+
+    const webClient = userPool.addClient("Web")
+
+    new sst.aws.Nextjs("Adona", {
+      path: "app",
+      link: [userPool],
+      environment: {
+        USER_POOL_ID: userPool.id,
+        USER_POOL_CLIENT_ID: webClient.id,
+      }
+    });
+  },
+});
+
